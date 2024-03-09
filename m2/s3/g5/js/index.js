8673b13bfb58e91f0cbb7ba30889b71366d7f034
IrCamel <dario03juve@gmail.com>
2024-03-09 16:45:46 +0100

    index js aggiornato

--- m2/s3/g5/js/index.js
+++ m2/s3/g5/js/index.js
@@ -1,33 +1,33 @@
-const apiKey = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhZGYwODJkN2IxMTAwMTkwZTZlMjAiLCJpYXQiOjE3MDk4OTEzMzYsImV4cCI6MTcxMTEwMDkzNn0.sINEtQxwEA-rQHSUfSGXGTYYHHya_4AgnXVe4tRC-2g";
+const apiKey =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhZGYwODJkN2IxMTAwMTkwZTZlMjAiLCJpYXQiOjE3MDk4OTEzMzYsImV4cCI6MTcxMTEwMDkzNn0.sINEtQxwEA-rQHSUfSGXGTYYHHya_4AgnXVe4tRC-2g";
 
-fetch("https://striveschool-api.herokuapp.com/api/product/"), {
-    Headers: {
-        method: "GET",
-        Authorization: apiKey,
-    }}
-    .then(res => res.json())
-    .then(telefono => {
-        for(let phone of telefono){
-            let cardPhone = creaClone()
+fetch("https://striveschool-api.herokuapp.com/api/product/", {
+  headers: {
+    method: "GET",
+    Authorization: apiKey
+    //"Content-Type": "application/json",
+  },
+})
+  .then((res) => res.json())
+  .then((telefono) => {
+    for (let phone of telefono) {
+      let cardPhone = creaClone();
 
-            let image = cardPhone.getElementById('imageUrl')
-            let name = cardPhone.getElementById('name')
-            let price = cardPhone.getElementById('price')
+      let image = cardPhone.querySelector('.card-img-top')
+      let name = cardPhone.querySelector('.card-title')
+      let description = cardPhone.querySelector('.card-text');
 
-            image.src = phone.imageUrl;
-            name.innerText = phone.name;
-            price.innerText = phone.price;
+      image.src = phone.imageUrl;
+      name.innerText = phone.name;
+      description.innerText = phone.description;
 
-            document.querySelector(".row").append(cardPhone)
-        }   
-    })
-
-    function creaClone(){
-        let template = document.querySelector('.cardProduct');
-        let clone = template.content.cloneNode(true);
-        console.log(template);
-            return clone
-
-            
+      document.querySelector(".row").append(cardPhone);
     }
+  });
 
+function creaClone() {
+  let template = document.querySelector(".cardProduct");
+  let clone = template.content.cloneNode(true);
+  console.log(template);
+  return clone;
+}
