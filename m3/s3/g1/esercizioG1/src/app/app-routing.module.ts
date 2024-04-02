9ec6cd00fe98cb5ab556af051868c721e7326df2
IrCamel <dario03juve@gmail.com>
2024-04-02 15:16:32 +0200

    m3 s3 g1

---
+++ m3/s3/g1/esercizioG1/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { AuthGuard } from './pages/account/guard/auth.guard';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+  loadChildren: () => import('./pages/homepage/homepage.module').then(m => m.HomepageModule)
+  },
+  {
+    path: 'chi-siamo',
+    loadChildren: () => import('./pages/chi-siamo/chi-siamo.module').then(m => m.ChiSiamoModule)
+  },
+  {
+    path: 'contatti',
+    loadChildren: () => import('./pages/contatti/contatti.module').then(m => m.ContattiModule),
+    canActivate:[AuthGuard]
+  },
+  { path: 'account',
+  loadChildren: () => import('./pages/account/account.module').then(m => m.AccountModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
