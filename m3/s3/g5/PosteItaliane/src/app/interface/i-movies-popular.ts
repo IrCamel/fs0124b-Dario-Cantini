4032534f5645c779004b4559fcd325dfc780161d
IrCamel <dario03juve@gmail.com>
2024-04-07 15:48:23 +0200

    reset progetto :'(

--- m3/s3/g5/PosteItaliane/src/app/interface/i-movies-popular.ts
+++
@@ -1,8 +0,0 @@
-export interface Root {
-  id: number
-  title: string
-  year: number
-  genres: string[]
-  plot: string
-  image: string
-}
--- m3/s3/g5/PosteItaliane/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  UsersUrl : "http://localhost:3000/users",
-  PopularUrl : "http://localhost:3000/movies-popular",
-  FavoritesUrl : "http://localhost:3000/favorites"
-};
